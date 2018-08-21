d6e131aa25c6c73f9bea648d8b7426f131b0c0a3
Jacob Ross <jross1818@gmail.com>
2018-08-21 18:14:21 -0500

    created a FizzBuzz output that works

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -16,6 +16,21 @@ public class FizzBuzz {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int num = 0;
+        int i = 0;
+        
+        for(i=1;i<101;i++){
+            if (i % 3 == 0 && i % 5 ==0){
+                System.out.print("FizzBuzz ");
+            } else if (i % 3 == 0){
+                System.out.print("Fizz ");
+            } else if (i % 5 == 0){
+                System.out.print("Buzz ");
+            }else {
+                System.out.print(i + " ");
+            }
+        }
+        
     }
     
 }
